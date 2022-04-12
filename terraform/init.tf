545b34dccedb6b4455e7fa0fdb2052f1fb1cd1eb
David Crespo <davidcrespoarroyo@gmail.com>
2022-04-12 21:14:01 +0100

    add gitignore and first terraform resource

--- terraform/init.tf
+++ terraform/init.tf
@@ -1,9 +1,17 @@
-provider "aws" {
-    profile = "default"
-    region = "eu-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-resource "aws_s3_bucket" "tf_course" {
-    bucket = "tf-course-20191118"
+provider "aws" {
+    profile = "default"
+}
+
+resource "aws_s3_bucket_acl" "tf_course" {
+    bucket = "tf-course-2022002"
     acl = "private"
 }
